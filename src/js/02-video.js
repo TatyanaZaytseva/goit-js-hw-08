35c6860f9a1879d9230a999eb25ed54f8f5654c3
TatyanaZaytseva <tatyana.lytvyniuk@gmail.com>
2022-08-21 23:41:34 +0300

    task 1-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+const player = new Player('vimeo-player');
+import throttle from 'lodash.throttle';
 
+const dataCurrentTime = {
+duration: 0,
+percent: 0,
+seconds: 0,
+}
+saveCurrentTime()
+    player.on('timeupdate', throttle((data) => localStorage.setItem('videoplayer-current-time', JSON.stringify(data)),1000));
+
+function saveCurrentTime() {
+    const savedCurrentTime = localStorage.getItem('videoplayer-current-time')
+    if (savedCurrentTime) {
+        dataCurrentTime.seconds = JSON.parse(savedCurrentTime).seconds;
+        dataCurrentTime.percent = JSON.parse(savedCurrentTime).percent;
+        dataCurrentTime.duration = JSON.parse(savedCurrentTime).duration;
+    }
+}
+
+player.setCurrentTime(dataCurrentTime.seconds);
